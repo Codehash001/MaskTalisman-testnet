1538644f1e218ef6a17b31b494edac507d89e05e
Codehash001 <codehashblockchain@gmail.com>
2023-03-29 03:42:18 +0530

    update

--- components/base.js
+++ components/base.js
@@ -20,7 +20,7 @@ const handleNav = () => {
   <>
    <div className='w-screen h-screen flex font-Archivo'>
    
-    <div className='w-[50%] h-screen p-5'>
+    <div className='w-[50%] h-screen px-5 pt-5'>
     	<div className='w-full h-[50px] px-8 flex justify-between items-center'>
     	  <img src='/Logo.jpeg' className='h-[40px]'/>
     	  <div className='flex items-center justify-end'>
@@ -32,7 +32,7 @@ const handleNav = () => {
     	<div className='w-full h-full px-8 flex flex-col justify-center items-center'>
     	  <h1 className='text-lg text-gray-800'>Welcome to</h1>
     	  <h1 className='text-black text-[64px] font-bold'>MaskTailsman</h1>
-    	  <h1 className='text-xl font-medium mt-6'>A must-see masterpiece that you won't want to miss</h1>
+    	  <h1 className='text-xl font-medium mt-3'>A must-see masterpiece that you won't want to miss</h1>
     	  <button type="button" className="mt-10 text-white dark:text-black font-semibold bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 rounded-sm text-sm px-5 py-2.5 text-center">
     	  Grab Your NFT now
     	  </button>
