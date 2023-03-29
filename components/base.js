6c0d7638a8bfdb0a77a129a6259e6678c88bb809
Codehash001 <codehashblockchain@gmail.com>
2023-03-29 20:40:35 +0530

    bug fixing

--- components/base.js
+++ components/base.js
@@ -29,13 +29,15 @@ const [toggleState, setToggleState] = useState(1);
    <div className='w-screen h-screen flex font-Archivo overflow-hidden'>
    
     <div className='w-full md:w-[50%] h-screen px-5 pt-5 filter backdrop-blur-sm md:backdrop-blur-none'>
-    	<div className='w-full h-[50px] px-8 flex justify-between items-center bg-white'>
+    	<div className='w-full h-[50px] md:px-8 px-2 flex justify-between items-center bg-white relative'>
     	  <img src='/Logo.jpeg' className='h-[40px]'/>
     	  <div className='flex items-center justify-end'>
     	    <h1 onClick={() => toggleTab(2)} className='text-[18px] font-medium cursor-pointer'>Overview</h1>
     	    <h1 onClick={() => toggleTab(3)} className='text-[18px] font-medium mx-5 cursor-pointer'>Mint</h1>
-    	    <AiOutlineMenu size={18} className='cursor-pointer'/>
+    	    <AiOutlineMenu size={18} className='cursor-pointer'/ onClick={handleNav}>
     	  </div>
+    	  
+    	  <div className={nav? 'absolute top-0 backdrop-blur-sm w-full h-full' : 'hidden'}></div>
     	</div>
     	
     	{
@@ -52,7 +54,7 @@ const [toggleState, setToggleState] = useState(1);
     	) :
     	(<div className='w-full h-full px-8 flex flex-col justify-start items-center mt-[150px]'>
     	  <h1 className='text-lg text-gray-800'>Welcome to</h1>
-    	  <h1 className='text-black md:text-[64px] text-3xl font-bold'>MaskTalisman</h1>
+    	  <h1 className='text-black md:text-[64px] text-3xl font-bold mt-2'>MaskTalisman</h1>
     	  <h1 className='text-xl font-medium mt-3'>A must-see masterpiece that you won't want to miss!</h1>
     	  <button type="button" className="mt-10 text-white dark:text-black font-semibold bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 rounded-sm text-sm px-5 py-2.5 text-center"
     	  onClick={() => toggleTab(3)}>
