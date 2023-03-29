a46cc2811fca560326056496f601386edbb90745
Codehash001 <codehashblockchain@gmail.com>
2023-03-29 11:41:59 +0530

    update

--- components/mint.js
+++ components/mint.js
@@ -17,17 +17,17 @@ export default function Mint () {
     	  
     	  <div className='w-full h-full flex rounded-md border mt-10'>
     	  
-    	    <div className='p-5 border-r'>
-    	      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#000'><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+    	    <div className='px-5 py-3 border-r'>
+    	      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#000'><path d="M5 11h14v2H5z"></path></svg>
     	    </div>
     	    
-    	    <div className='py-5 px-20 border-r'><h1 className='text-lg'> 1 </h1></div>
+    	    <div className='py-3 px-20 border-r'><h1 className='text-lg'> 1 </h1></div>
     	    
-    	    <div className='p-5 border-r'>
-    	       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#000'><path d="M5 11h14v2H5z"></path></svg>
+    	    <div className='px-5 py-3 border-r'>
+    	       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#000'><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
     	    </div>
     	    
-    	    <div className='p-5 bg-black text-white'> Mint </div>
+    	    <div className='px-10 py-3 bg-black text-white'> Mint </div>
     	    
     	  </div>
     	</div>
@@ -37,4 +37,6 @@ export default function Mint () {
   </>
   )
 }
+
+
 
