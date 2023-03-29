fccb65f6c20db1352eb9d7ad136b5dc08df8678b
Codehash001 <codehashblockchain@gmail.com>
2023-03-29 12:56:07 +0530

    update

--- components/mint.js
+++ components/mint.js
@@ -15,9 +15,19 @@ export default function Mint () {
     <div className='w-full h-full p-20 flex flex-col justify-center items-center rounded-lg border border-black-2'>
     	  <h1 className='text-black text-[30px] font-bold'>Mint is Live!</h1>
     	  
-    	<div className='w-full h-full flex justify-center items-center rounded-md border mt-10'> 
-    	<img src='/logoCL.jpeg' className='h-[240px]'/>
-    	</div>
+    	  <div className='w-auto flex justify-center items-center'>
+                <div className="z-10 absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-purple rounded-md flex items-center justify-center text-white font-semibold">
+                  <p>
+                    1 / 335                    
+                  </p>
+                </div>
+
+                <img
+		  alt="image"
+                  src="/logoCL.jpeg"
+                  className="object-cover w-full mt-auto mb-0 sm:h-[280px] md:w-[250px] rounded-md border border-gray-100"
+                />
+    	  </div>
     	  
     	  <div className='w-full h-full flex rounded-md border mt-10'>    	  
     	    <div className='px-5 py-3 border-r'>
